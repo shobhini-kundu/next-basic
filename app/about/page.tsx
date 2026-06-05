96e0ae1e528a587f64a49f58e6c55c298a636ed5
Shobhini <shobhinikundu@gmail.com>
2026-06-05 18:20:04 +0530

    page created

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutVariable from "@/components/aboutComponents/AboutVariable";
+import React from "react";
+
+const About: React.FC = () => {
+    return (<>
+    <AboutVariable/>
+
+    </>);
+}
+export default About;
---
+++ components/aboutComponents/AboutVariable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutVariable = () => {
+  return (
+    <div>AboutVariable</div>
+  )
+}
+
+export default AboutVariable
